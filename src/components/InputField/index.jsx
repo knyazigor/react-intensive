038fef8599fc5f74432168b9fd0981637f65f597
Igor Shaymukhametov <shaym@yandex.ru>
2023-10-16 14:49:19 +0500

    refactor: some code style improvements

--- src/components/InputField/index.jsx
+++ src/components/InputField/index.jsx
@@ -10,19 +10,20 @@ class InputField extends React.Component {
       value: '',
     };
     this.input = React.createRef();
+    console.log('constructor hook');
   }
 
   handleFocus = () => {
-    this.setState({ ...this.state, focused: true });
+    this.setState({ focused: true });
   }
 
   handleBlur = () => {
-    this.setState({ ...this.state, focused: false });
+    this.setState({ focused: false });
   }
 
   handleClear = (e) => {
     this.input.current.focus();
-    this.setState({ ...this.state, value: '' });        
+    this.setState({ value: '' });        
   }
 
   handleSearch = (e) => {
@@ -31,18 +32,15 @@ class InputField extends React.Component {
   }
 
   handleChange = (e) => {
-    this.setState({ ...this.state, value: e.target.value });
+    this.setState({ value: e.target.value });
   }
 
   handleKeyDown = ({ key }) => {
-    switch(key) {
-      case 'Escape': 
-        this.handleClear();
-        break;
-      case 'Enter': 
-        this.handleSearch();
-        break;
-      // no default 
+    if (key === 'Escape') {
+      this.handleClear();
+    } 
+    if (key === 'Enter') {
+      this.handleSearch();
     }
   }
 
@@ -60,20 +58,22 @@ class InputField extends React.Component {
 
   render() {
     return (
-      <div className='input-field__wrapper'>
-        <InputFieldBtn name='cancel' hidden={!this.state.value} handleClick={this.handleClear} />
-        <input 
-          type='text' 
-          className='input-field' 
-          value={this.state.value}
-          ref={this.input} 
-          onFocus={this.handleFocus} 
-          onBlur={this.handleBlur} 
-          onChange={this.handleChange}
-          onKeyDown={this.handleKeyDown}
-        />
-        <InputFieldBtn name='double_arrow' handleClick={this.handleSearch} />
-      </div>
+      <>
+        <div className='input-field__wrapper'>
+          <InputFieldBtn name='cancel' hidden={!this.state.value} handleClick={this.handleClear} />
+          <input 
+            type='text' 
+            className='input-field' 
+            value={this.state.value}
+            ref={this.input} 
+            onFocus={this.handleFocus} 
+            onBlur={this.handleBlur} 
+            onChange={this.handleChange}
+            onKeyDown={this.handleKeyDown}
+          />
+          <InputFieldBtn name='double_arrow' handleClick={this.handleSearch} />
+        </div>
+      </>
     )
   } 
 }
--- src/components/InputFieldBtn/index.jsx
+++ src/components/InputFieldBtn/index.jsx
@@ -5,11 +5,11 @@ class InputFieldBtn extends React.Component {
   render() {
     return (
       <button 
-        className={`material-symbols-outlined input-field__btn ${this.props.hidden && 'input-field__btn_hidden'}`}
-        onClick={this.props.handleClick}
-      >
-        {this.props.name}
-      </button>
+          className={`material-symbols-outlined input-field__btn ${this.props.hidden && 'input-field__btn_hidden'}`}
+          onClick={this.props.handleClick}
+        >
+          {this.props.name}
+        </button>
     )
   } 
 }
